ecdce707fc6620cce0886ae929d04eb7b26cd96d
Diogo Neves <diogo.neves@gmail.com>
2012-01-19 23:41:40 +0000

    Renamed Main to a more sensible name

--- SmasherConsole/SmasherConsole.cs
+++ SmasherConsole/SmasherConsole.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace SmasherConsole
 {
-	class MainClass
+	class Smasher
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine("Hello World!");
 		}
 	}
 }
