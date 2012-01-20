70f264c0ab4a40125c316b9361acd1858a7f6e6e
Diogo Neves <diogo.neves@gmail.com>
2012-01-20 17:24:01 +0000

    Created simple server
    - Created node.js test
    - Added request from Csharp
    - Exposed add, remove and list smashers to the API

--- SmasherConsole/SmasherConsole.cs
+++ SmasherConsole/SmasherConsole.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 namespace SmasherConsole
 {
@@ -6,7 +7,14 @@ namespace SmasherConsole
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Starting Smasher!");
+
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://127.0.0.1:1337/addsmasher");
+			request.Method = "POST";
+
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+			Console.WriteLine(response.ToString());
 		}
 	}
 }
