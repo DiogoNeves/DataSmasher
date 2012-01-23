4f6e03af6fec9323da41552f22a9bab3c8b380ef
Diogo Neves <diogo.neves@gmail.com>
2012-01-23 15:25:56 +0000

    Updated home page
    - Added link to list

--- SmasherServer/routes/index.js
+++ SmasherServer/routes/index.js
@@ -4,9 +4,9 @@
  */
 
 exports.index = function(req, res) {
-  res.render('index', { title: 'DataSmasher Server' });
+  res.render('index', { title: 'DataSmasher Server', server: '127.0.0.1:3000' });
 };
 
 exports.missing = function(req, res) {
-	res.render('index', { title: 'DataSmasher Server - invalid parameters' });
+	res.render('index', { title: 'DataSmasher Server - invalid parameters', server: '127.0.0.1:3000' });
 };
